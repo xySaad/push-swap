ba9db4c156dffb1fda8fab7b8034e38cc2e6c0c2
srm <saadrm.dev@gmail.com>
2024-12-11 11:04:15 +0100

    feat: store instructions in list Struct

--- utils/instructions.go
+++ utils/instructions.go
@@ -1,10 +1,13 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type List struct {
-	A stack
-	B stack
+	A            stack
+	B            stack
+	Instructions []string
 }
 
 func (l *List) Print() {
@@ -32,58 +35,72 @@ func (l *List) PrintR() {
 }
 
 func NewList(slice []int) List {
-	return List{NewStack(slice), NewStack(nil)}
+	return List{NewStack(slice, true), NewStack(nil, true), []string{}}
+}
+func NewUntrackedList(slice []int) List {
+	return List{NewStack(slice, false), NewStack(nil, false), []string{}}
 }
 
 func (l *List) Sa() {
-	fmt.Println("sa")
+	l.Instructions = append(l.Instructions, "sa")
 	l.A.swap()
 }
 func (l *List) Sb() {
-	fmt.Println("sb")
+	l.Instructions = append(l.Instructions, "sb")
 	l.B.swap()
 }
 func (l *List) Pa() {
-	fmt.Println("pa")
+	l.Instructions = append(l.Instructions, "pa")
 	l.A.push(l.B.pop())
 }
 func (l *List) Pb() {
-	fmt.Println("pb")
+	l.Instructions = append(l.Instructions, "pb")
 	l.B.push(l.A.pop())
 }
 
 func (l *List) Ra() {
-	fmt.Println("ra")
+	l.Instructions = append(l.Instructions, "ra")
 	l.A.rotate()
 }
 func (l *List) Rb() {
-	fmt.Println("rb")
+	l.Instructions = append(l.Instructions, "rb")
 	l.B.rotate()
 }
 
 func (l *List) Rr() {
-	fmt.Println("rr")
+	l.Instructions = append(l.Instructions, "rr")
 	l.A.rotate()
 	l.B.rotate()
 }
 
 func (l *List) Rra() {
-	fmt.Println("rra")
+	l.Instructions = append(l.Instructions, "rra")
 	l.A.reverseRotate()
 }
 func (l *List) Rrb() {
-	fmt.Println("rrb")
+	l.Instructions = append(l.Instructions, "rrb")
 	l.B.reverseRotate()
 }
 
 func (l *List) Rrr() {
-	fmt.Println("rrr")
+	l.Instructions = append(l.Instructions, "rrr")
 	l.A.reverseRotate()
 	l.B.reverseRotate()
 }
 
 func (l *List) Ss() {
-	fmt.Println("ss")
+	l.Instructions = append(l.Instructions, "ss")
 	l.A.swap()
 	l.B.swap()
+}
+
+func (l *List) Do(operation string) {
+	switch operation {
+	case "ra":
+		l.Ra()
+	case "pb":
+		l.Pb()
+	case "pa":
+		l.Pa()
+	}
 }
